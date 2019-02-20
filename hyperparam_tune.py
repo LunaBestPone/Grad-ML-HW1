2b51964b756bbd69708c53b5744f34a4ad2ff10e
LunaBestPone <saltyprinx@gmail.com>
2019-02-20 00:18:40 -0600

    digit case final accuracy off by -1, can't fix won't fix

--- hyperparam_tune.py
+++ hyperparam_tune.py
@@ -1,6 +1,7 @@
 import sys, json
 import numpy as np
 from collections import OrderedDict
+from operator import itemgetter
 
 def main():
     if len(sys.argv) != 5:
@@ -63,64 +64,29 @@ def main():
 
     if training_set_vote_feature_matrix.size != 0 :
         feature_status[1] = 1
-
     # accuracy stored in this column vector, later can be stable sorted
     k_acc_matrix = np.zeros((max_k,1))
 
-    if feature_status == [1,0]:
-        for k in range(1,max_k+1):
-            for i in range(normed_val_set_num_feature_matrix.shape[0]):
+    for k in range(1,max_k+1):
+        for i in range(val_set_label_matrix.shape[0]):
 
-                dict_count = OrderedDict()
-                for label in training_set["metadata"]["features"][-1:][0][1]:
-                    dict_count[label] = 0
-
-                num_d = np.absolute(normed_training_set_num_feature_matrix - normed_val_set_num_feature_matrix[i])
-                num_d = np.sum(num_d, 1)
-                knn_index_list = np.argsort(num_d,axis=0,kind="stable")[:k]
-                knn_label_list = training_set_label_matrix[knn_index_list]
-                for label in knn_label_list:
-                    dict_count[label[0]] += 1
-                max_count_label = max(dict_count,key=dict_count.get)
-                if(max_count_label == val_set_label_matrix[i][0]):
-                    k_acc_matrix[k-1] += 1
-
-    if feature_status == [0,1]:
-        for k in range(1,max_k+1):
-            for i in range(val_set_vote_feature_matrix.shape[0]):
-
-                dict_count = OrderedDict()
-                for label in training_set["metadata"]["features"][-1:][0][1]:
-                    dict_count[label] = 0
-
-                vote_d = np.sum(val_set_vote_feature_matrix[i] != training_set_vote_feature_matrix, 1)
-                knn_index_list = np.argsort(vote_d,axis=0,kind="stable")[:k]
-                knn_label_list = training_set_label_matrix[knn_index_list]
-                for label in knn_label_list:
-                    dict_count[label[0]] += 1
-
-                max_count_label = max(dict_count,key=dict_count.get)
-                if(max_count_label == val_set_label_matrix[i][0]):
-                    k_acc_matrix[k-1][0] += 1
-
-    if feature_status == [1,1]:
-        for k in range(1,max_k+1):
-            for i in range(val_set_vote_feature_matrix.shape[0]):
-
-                dict_count = OrderedDict()
-                for label in training_set["metadata"]["features"][-1:][0][1]:
-                    dict_count[label] = 0
-                num_d = np.absolute(normed_training_set_num_feature_matrix - normed_val_set_num_feature_matrix[i])
-                num_d = np.sum(num_d, 1)
-                vote_d = np.sum(val_set_vote_feature_matrix[i] != training_set_vote_feature_matrix, 1)
-                total_d = num_d + vote_d
-                knn_index_list = np.argsort(total_d,axis=0,kind="stable")[:k]
-                knn_label_list = training_set_label_matrix[knn_index_list]
-                for label in knn_label_list:
-                    dict_count[label[0]] += 1
-                max_count_label = max(dict_count,key=dict_count.get)
-                if(max_count_label == val_set_label_matrix[i][0]):
-                    k_acc_matrix[k-1][0] += 1
+            dict_count = OrderedDict()
+            for label in training_set["metadata"]["features"][-1:][0][1]:
+                dict_count[label] = 0
+            d = 0
+            if feature_status == [1,0]:
+                d = np.sum(np.absolute(normed_training_set_num_feature_matrix - normed_val_set_num_feature_matrix[i]), 1)
+            if feature_status == [0,1]:
+                d = np.sum(val_set_vote_feature_matrix[i] != training_set_vote_feature_matrix, 1)
+            if feature_status == [1,1]:
+                d = np.sum(np.absolute(normed_training_set_num_feature_matrix - normed_val_set_num_feature_matrix[i]), 1) + np.sum(val_set_vote_feature_matrix[i] != training_set_vote_feature_matrix, 1)
+            knn_index_list = np.argsort(d,axis=0,kind="stable")[:k]
+            knn_label_list = training_set_label_matrix[knn_index_list]
+            for label in knn_label_list:
+                dict_count[label[0]] += 1
+            max_count_label = sorted(dict_count.items(),key=itemgetter(1),reverse=True)[0][0]
+            if(max_count_label == val_set_label_matrix[i][0]):
+                k_acc_matrix[k-1] += 1
 
     k_acc_matrix = k_acc_matrix/float(val_set_label_matrix.shape[0])
     for i in range(k_acc_matrix.shape[0]):
@@ -128,9 +94,7 @@ def main():
     best_k = np.argsort((-k_acc_matrix),axis=0,kind="stable")[0][0] + 1
     best_k_acc = 0
 
-    print(normed_training_set_num_feature_matrix.shape[0])
     normed_training_set_num_feature_matrix = np.vstack((normed_training_set_num_feature_matrix,normed_val_set_num_feature_matrix))
-    print(normed_training_set_num_feature_matrix.shape[0])
     training_set_vote_feature_matrix = np.vstack((training_set_vote_feature_matrix,val_set_vote_feature_matrix))
     training_set_label_matrix = np.vstack((training_set_label_matrix,val_set_label_matrix))
 
@@ -139,7 +103,6 @@ def main():
         dict_count = OrderedDict()
         for label in training_set["metadata"]["features"][-1:][0][1]:
             dict_count[label] = 0
-
         d = 0
         if feature_status == [1,0]:
             d = np.sum(np.absolute(normed_training_set_num_feature_matrix - normed_test_set_num_feature_matrix[i]), 1)
@@ -151,12 +114,10 @@ def main():
         knn_label_list = training_set_label_matrix[knn_index_list]
         for label in knn_label_list:
             dict_count[label[0]] += 1
-        max_count_label = max(dict_count,key=dict_count.get)
+        max_count_label = sorted(dict_count.items(),key=itemgetter(1),reverse=True)[0][0]
         if(max_count_label == test_set_label_matrix[i][0]):
             best_k_acc += 1
 
-    print(best_k_acc)
-    print(test_set_label_matrix.shape[0])
     best_k_acc = float(best_k_acc)/float(test_set_label_matrix.shape[0])
     print(str(best_k))
     print(str(best_k_acc))
