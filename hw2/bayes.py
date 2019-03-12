684cc39d35492ad3d7a05750b436eeb578a52f0b
LunaBestPone <saltyprinx@gmail.com>
2019-03-11 20:51:45 -0500

    modified directory structure, added hw2 init code and data

---
+++ hw2/bayes.py
@@ -0,0 +1,13 @@
+import sys, json
+import numpy as np
+
+def main():
+    # simple arg check
+    if len(sys.argv) != 4:
+        print("wrong number of arguments")
+        sys.exit(0)
+
+
+
+if __name__ == "__main__":
+    main()
